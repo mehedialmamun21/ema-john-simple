4eff8c05d87ad18780357435554b9f07eff03f45
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-02 02:04:42 +0600

    successfully showing data from local storage when reload

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
+    // console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+
+    const grandTotal = total + shipping + tax;
 
     return (
-        <div>
-            <h3>Order summary</h3>
-            <p>Selected Items : {cart.length}</p>
+        <div className='cart'>
+            <h2>Order summary</h2>
+            <p>Selected Items : {quantity}</p>
+            <p>Total price : ${total} </p>
+            <p>Total Shipping Charge : ${shipping} </p>
+            <p>Tax (10%) : ${tax}  </p>
+            <h4>Grand Total : $ {grandTotal.toFixed(2)} </h4>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,24 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+
     const [cart, setCart] = useState([]);
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
-    // console.log(products);
 
-    const handleAddToCart = (product) => {
-        // console.log(product);
-        // cart.push(product);
-        const newCart = [...cart, product];
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart = (selectedProduct) => {
+        // console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        } else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+
         setCart(newCart);
-        // console.log(cart);
+        addToDb(selectedProduct.id);
     }
 
     return (
@@ -41,52 +66,3 @@ const Shop = () => {
 };
 
 export default Shop;
-
-/* 
-import React, { useState } from 'react';
-import Product from '../Product/Product';
-import './Shop.css'
-
-const Shop = () => {
-    const [cart, setCart] = useState([]);
-    const products = [
-        { id: 1, name: 'asus wat', img: "images/lap1.jpg", price: 75000 },
-        { id: 2, name: 'hp fet', img: "images/lap2.jpg", price: 70000 },
-        { id: 3, name: 'del wex', img: "images/lap3.jpg", price: 68000 },
-        { id: 4, name: 'asus xoo', img: "images/lap4.jpg", price: 65000 },
-        { id: 5, name: 'lenovo wisten', img: "images/lap5.jpg", price: 60000 },
-        { id: 6, name: 'hp pavilion', img: "images/lap6.jpg", price: 55000 },
-        { id: 7, name: 'apple mac', img: "images/lap7.jpg", price: 50000 },
-        { id: 8, name: 'hp autt', img: "images/lap8.jpg", price: 45000 },
-        { id: 9, name: 'asus zenbook', img: "images/lap9.jpg", price: 45000 }
-    ]
-    const handleAddToCart = (product) => {
-        console.log(product);
-        // cart.push(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
-    }
-    return (
-        <div className='shop-container'>
-
-            <div className='products-container'>
-                {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-
-            <div className='cart-container'>
-                <h2>Order Summary</h2>
-                <p>selected items : {cart.length}</p>
-            </div>
-
-        </div>
-    );
-};
-
-export default Shop;
-*/
